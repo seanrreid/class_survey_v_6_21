b02a9b9ca275a0eea3d73e67022c9fbed046eeee
Sean Reid <sean@torchcodelab.com>
2021-06-25 17:21:35 -0400

    fixed the missing ClassSurveyModel changes that were accidentally erased

--- models/ClassSurveyModel.js
+++ models/ClassSurveyModel.js
@@ -10,16 +10,39 @@ class ClassSurveyModel {
     static async getAllTopicData() {
         try {
             const response = await db.any(
-                `SELECT * FROM topics
+                `SELECT topic_name, topic_score, ranking_title FROM topics
                     JOIN rankings
                     ON topics.topic_score = rankings.id
                 ORDER BY topics.topic_name;
                 `
-            )
+            );
             return response;
         } catch (error) {
             console.error('ERROR: ', error);
             return error;
+        }
+    }
+    static async getAllRankings() {
+        try {
+            const response = await db.any(
+                `SELECT * FROM rankings;`
+            );
+            return response;
+        } catch (error) {
+            console.error('ERROR: ', error);
+            return error;
+        }
+    }
+
+    static async updateRanking(topic, rank_id) {
+        try {
+            const response = await db.result(
+                `UPDATE topics SET topic_score = $1 WHERE topic_name = $2;`, [rank_id, topic]
+            );
+            return response;
+        } catch (err) {
+            console.log('ERROR', err.message);
+            return err;
         }
     }
 }
