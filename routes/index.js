d292b8fb2026f4c2b214cd985b719d39784147ce
Sean Reid <sean@torchcodelab.com>
2021-06-25 12:42:11 -0400

    Added route handlers for POST request and data Models to GET ranking info, and UPDATEING scores for topics

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,53 @@ const ClassSurveyModel = require('../models/ClassSurveyModel');
 
 router.get('/', async (req, res) => {
     const topicData = await ClassSurveyModel.getAllTopicData();
+    const rankings = await ClassSurveyModel.getAllRankings();
+
     res.render('template', {
         locals: {
             title: 'Class Survey',
-            data: topicData
+            data: topicData,
+            rankings: rankings,
         },
         partials: {
-            body: 'partials/home'
+            body: 'partials/home',
+        },
+    });
+});
+
+router.get('/error', async (req, res) => {
+    const { error } = req.query;
+
+    let errorMessage = error;
+
+    if (!error) {
+        errorMessage = "No Error Message Defined";
+    }
+
+    res.render('template', {
+        locals: {
+            title: "ERROR PAGE",
+            errorMessage: errorMessage
+        },
+        partials: {
+            body: 'partials/error'
         }
     })
+})
+
+router.post('/update', async (req, res) => {
+    let response;
+
+    for (let key in req.body) {
+        console.log('KEY AND VALUE:', key, req.body[key]);
+        response = await ClassSurveyModel.updateRanking(key, req.body[key]);
+    }
+
+    if (response.rowCount !== 1) {
+        res.redirect(`/error?error=${response}`);
+    } else {
+        res.redirect('/');
+    }
 });
 
 module.exports = router;
