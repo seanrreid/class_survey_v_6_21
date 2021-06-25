8a52d40fa2771a3636c7baa07e3babcd3878061e
Sean Reid <sean@torchcodelab.com>
2021-06-25 12:56:03 -0400

    Setup app to read from static directory public

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
